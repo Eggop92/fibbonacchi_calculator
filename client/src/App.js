932b0e6359aca2a1411c59c64d5c5d87242d0dc2
e.puerta <e.puerta@speexx.com>
2025-08-07 08:26:07 +0200

    Routing in the React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { Link, Route, BowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Fib from './Fib';
+import logo from './logo.svg';
+import OtherPage from './OtherPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherPage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherPage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
